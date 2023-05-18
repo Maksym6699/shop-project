e0fa4c5a4da600549d56737b86af2567e5d0dfd4
Maks <kozyrmaxim20@gmail.com>
2023-05-18 20:24:33 +0300

    create App document

--- src/index.tsx
+++ src/index.tsx
@@ -7,17 +7,41 @@ import ReactDOM from 'react-dom/client'
 // const app = React.createElement("div", null, React.createElement('p', {id: 'test',}, 'Hello p'))
 
 // JSX
-const app = (
-    <div>
-        <h1 id="test">Hello App</h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-            aliquid explicabo iusto ratione natus voluptatibus vero labore
-            error. Laboriosam neque corporis explicabo? Pariatur libero veniam
-            fuga atque dolore expedita impedit.
-        </p>
-    </div>
-)
+// const app = (
+//     <div>
+//         <h1 id="test">Hello App</h1>
+//         <p>
+//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+//             aliquid explicabo iusto ratione natus voluptatibus vero labore
+//             error. Laboriosam neque corporis explicabo? Pariatur libero veniam
+//             fuga atque dolore expedita impedit.
+//         </p>
+//     </div>
+// )
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello App Component</h1>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga,
+                repudiandae. Asperiores voluptatem dicta rerum quasi tempora?
+                Optio exercitationem rem ullam iste, dicta quasi facilis nam,
+                corporis dolor fugit inventore? Libero.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque
+                animi sint labore maxime at quia iusto. Possimus ducimus quas,
+                doloremque beatae omnis unde itaque voluptas velit aliquam porro
+                quae dignissimos.
+            </p>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{app}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
