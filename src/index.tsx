f73b647bd5b3b2e6d24fba135aab2aba497088cb
Maks <kozyrmaxim20@gmail.com>
2023-05-18 20:03:16 +0300

    create React element

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// React element
+const title = React.createElement('h1', { id: 'title' }, 'Hello React')
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{title}</React.StrictMode>)
