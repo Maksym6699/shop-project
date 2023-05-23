590cf008daabf11e1bcdaf70f1617a7f1e785f5c
Maks <kozyrmaxim20@gmail.com>
2023-05-23 20:42:30 +0300

    added absolutely import TSX

---
+++ src/Container/Header/Header.tsx
@@ -0,0 +1,6 @@
+// tsrafce - створення нового документу
+type Props = {}
+const Header = (props: Props) => {
+    return <div>Header</div>
+}
+export default Header
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './style.css'
-import Title from './Title'
-import Content from './Content'
-
-const App = () => {
-    return (
-        <>
-            <Title title="App" />
-            <Title title="React" num={1} />
-            <Content
-                text1="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga, repudiandae. Asperiores voluptatem dicta rerum quasi tempora? Optio exercitationem rem ullam iste, dicta quasi facilis nam,corporis dolor fugit inventore? Libero."
-                text2="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga, repudiandae. Asperiores voluptatem dicta rerum quasi tempora? Optio exercitationem rem ullam iste, dicta quasi facilis nam,corporis dolor fugit inventore? Libero."
-            />
-        </>
-    )
-}
+import App from 'Container/App/App'
+import Header from 'Container/Header/Header'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
@@ -23,6 +9,3 @@ root.render(
         <App />
     </React.StrictMode>
 )
-
-// 1. Винести Content в окремий файл
-// 2. Описати дві прописи text1, text2, які мають бути string і передати туди дані
