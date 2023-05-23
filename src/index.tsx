b611c4197fe0495e1b69c7b4b16620185a701664
Maks <kozyrmaxim20@gmail.com>
2023-05-23 20:11:39 +0300

    New file Content.tsx import\export connect to index

--- src/index.tsx
+++ src/index.tsx
@@ -2,32 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
 import Title from './Title'
-
-const Content = () => {
-    return (
-        <React.Fragment>
-            <p className="red">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga,
-                repudiandae. Asperiores voluptatem dicta rerum quasi tempora?
-                Optio exercitationem rem ullam iste, dicta quasi facilis nam,
-                corporis dolor fugit inventore? Libero.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque
-                animi sint labore maxime at quia iusto. Possimus ducimus quas,
-                doloremque beatae omnis unde itaque voluptas velit aliquam porro
-                quae dignissimos.
-            </p>
-        </React.Fragment>
-    )
-}
+import Content from './Content'
 
 const App = () => {
     return (
         <>
             <Title title="App" />
             <Title title="React" num={1} />
-            <Content />
+            <Content
+                text1="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga, repudiandae. Asperiores voluptatem dicta rerum quasi tempora? Optio exercitationem rem ullam iste, dicta quasi facilis nam,corporis dolor fugit inventore? Libero."
+                text2="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga, repudiandae. Asperiores voluptatem dicta rerum quasi tempora? Optio exercitationem rem ullam iste, dicta quasi facilis nam,corporis dolor fugit inventore? Libero."
+            />
         </>
     )
 }
@@ -38,3 +23,6 @@ root.render(
         <App />
     </React.StrictMode>
 )
+
+// 1. Винести Content в окремий файл
+// 2. Описати дві прописи text1, text2, які мають бути string і передати туди дані
