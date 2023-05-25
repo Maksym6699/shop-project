101b923134aa370adff5e7c80cd8114cd97ab897
Maks <kozyrmaxim20@gmail.com>
2023-05-25 20:49:41 +0300

    added Home

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -1,10 +1,12 @@
 import Header from 'Container/Header/Header'
 import { StyledEngineProvider } from '@mui/material/styles'
+import Main from 'Container/Main/Main'
 
 const App = () => {
     return (
         <StyledEngineProvider injectFirst>
             <Header />
+            <Main />
         </StyledEngineProvider>
     )
 }
