4c22048488dcffd50e0533f90136eae23ea65b1e
Maks <kozyrmaxim20@gmail.com>
2023-05-25 20:07:25 +0300

    added Logo, Menu, components

--- src/Container/Header/Header.tsx
+++ src/Container/Header/Header.tsx
@@ -1,8 +1,35 @@
-// tsrafce - створення нового документу
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
 import './Header.scss'
+import { Container } from '@mui/material'
+import Logo from 'components/Logo/Logo'
+import Menu from 'Menu/Menu'
 
 type Props = {}
+
 const Header = (props: Props) => {
-    return <div className="red">Header</div>
+    return (
+        <AppBar position="static">
+            <Container>
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Logo />
+                    <Menu />
+                </Toolbar>
+            </Container>
+        </AppBar>
+    )
 }
+
 export default Header
