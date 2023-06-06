6ceb78438b88732bbf693050511be486151a9b8f
Maks <kozyrmaxim20@gmail.com>
2023-06-06 19:35:59 +0300

    added  disabled click limits and onIncrementClick onDecrementClick

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -6,6 +6,7 @@ import {
     TextField,
 } from '@mui/material'
 import './ProductsListItem.scss'
+import { useState } from 'react'
 
 type Props = {
     title: string
@@ -23,6 +24,13 @@ const ProductsListItem = ({
     price,
     image,
 }: Props) => {
+    const [count, setCount] = useState<number>(1)
+    const onIncrementClick = () => {
+        setCount((prevState) => prevState + 1)
+    }
+    const onDecrementClick = () => {
+        setCount((prevState) => prevState - 1)
+    }
     return (
         <Card className="product" variant="outlined">
             <CardContent>
@@ -35,9 +43,21 @@ const ProductsListItem = ({
                 <div className="product-features">Capacity: {capacity}Gb</div>
                 <div className="product-price">Price: $ {price}</div>
                 <div className="product-quantity">
-                    <Button variant="outlined">-</Button>
-                    <TextField value="1" size="small" />
-                    <Button variant="outlined">+</Button>
+                    <Button
+                        variant="outlined"
+                        onClick={onDecrementClick}
+                        disabled={count <= 1}
+                    >
+                        -
+                    </Button>
+                    <TextField value={count} size="small" />
+                    <Button
+                        variant="outlined"
+                        onClick={onIncrementClick}
+                        disabled={count >= 10}
+                    >
+                        +
+                    </Button>
                 </div>
                 <CardActions className="btn-wrap">
                     <Button variant="outlined">Add to cart</Button>
